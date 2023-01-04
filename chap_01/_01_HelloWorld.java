7db33d6d7949d8f06f92ca1563b278ac7670061e
이준범 <97700761+j-baam@users.noreply.github.com>
2023-01-05 00:37:57 +0900

    Delete chap_01 directory

--- chap_01/_01_HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package chap_01; //패키지
-
-public class _01_HelloWorld {
-    public static void main(String[] agrs) {  //Main에 있는 {}실행
-        System.out.println("안녕하세요");  //println line
-    }
-}
